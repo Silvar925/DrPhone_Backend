e61246872bd61d58943f24ee8f62a379f39fc91b
Silvar925 <silvarmoonlight@gmail.com>
2024-05-28 17:29:10 +0300

    2.1

--- drphone/Core/models.py
+++ drphone/Core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
+import unidecode
 
 
 class ColorProduct(models.Model):
@@ -24,6 +26,16 @@ class ImagesProduct(models.Model):
         return f"{self.name}"
 
 
+def generateUniqueSlug(instance, new_slug=None):
+    slug = new_slug if new_slug else slugify(unidecode.unidecode(instance.name))
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(unique_id=slug).exists()
+    if qs_exists:
+        new_slug = f"{slug}-{Klass.objects.filter(unique_id__startswith=slug).count() + 1}"
+        return generateUniqueSlug(instance, new_slug=new_slug)
+    return slug
+
+
 class Product(models.Model):
     SIM_CHOICES = [
         ('SIM_SIM', 'SIM + SIM'),
@@ -39,12 +51,19 @@ class Product(models.Model):
         ('2048Gb','2048Gb'),
     ]
 
+    id = models.SlugField(max_length=255, unique=True, editable=False)
+
     name = models.CharField(max_length=50, verbose_name='Название товара')
     color = models.ManyToManyField(ColorProduct, verbose_name="Цвет")
     memory = models.CharField(max_length=8, choices=MEMORY_CHOICES, blank=True, null=True, verbose_name='Память')
     images = models.ManyToManyField(ImagesProduct, verbose_name="Изображение")
     sim = models.CharField(max_length=20, choices=SIM_CHOICES, blank=True, null=True, verbose_name='СИМ КАРТА')
     price = models.IntegerField(verbose_name="Цена", blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = generateUniqueSlug(self)
+        super().save(*args, **kwargs)
 
 
     class Meta:
--- drphone/drphone/settings.py
+++ drphone/drphone/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'Core.apps.CoreConfig'
 ]
@@ -38,6 +39,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'drphone.urls'
@@ -118,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#API sucsesfull
+CORS_ALLOW_ALL_ORIGINS = True
